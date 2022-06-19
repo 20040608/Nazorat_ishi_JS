96170ef4dfcbbb93e622a99786c7ff8badd12d23
Sherkhon <issakvell@gmail.com>
2022-06-19 17:02:43 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,82 @@
+// Masala---TaskOne------------------
+
+// var numbers = [1, 3, 0, 5, 0, 7, 0];
+
+// function func(array) {
+// 	var zero = [];
+
+// 	for (num of array) {
+// 		if (num == 0) {
+// 			zero.push(num);
+// 		} else {
+// 			zero.unshift(num);
+// 		}
+// 	}
+// 	return zero;
+// }
+
+// console.log(func(numbers));
+
+
+
+// Masala---TaskTwo------------
+
+// var countTrue = [false, true, false, true, true];
+
+// var count = true;
+
+// var counter = 0;
+
+// for (num of countTrue){
+
+//   if(num == count){
+//     counter++
+//   }
+  
+// }
+
+// console.log(counter);
+
+
+
+// Masala---TaskThree------------------
+
+
+// var numbers = [1, 2, 0, -1, -2, 3, 4];
+
+// function addition(array) {
+// 	var count = 0;
+
+// 	for (num of array) {
+//     if(num > 0) {
+//         count = count + num
+//     } 
+// 	}
+// 	return count;
+// }
+
+// console.log(addition(numbers));
+
+
+
+// Masala---TaskFour-----------------
+
+
+// var obj = {};
+// var sas = {};
+// var noun = {};
+
+// function fiftyThirtyTwenty(price) {
+//   var obj = {
+//     Ali : price = 200
+//   }
+//   var sas = {
+//     Umid : price = 800
+//   }
+//   var noun = {
+//     Sardor : price = 400
+//   } 
+//   return sas;
+// }
+
+// console.log(fiftyThirtyTwenty());    
